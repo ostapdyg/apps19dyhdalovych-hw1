c694b745a3f096fdbb8b89bd727b639a543a6da0
ostapdyg <dyhdalovych@ucu.edu.ua>
2019-10-18 19:55:26 +0300

    Deviation test

--- src/main/java/ua/edu/ucu/tempseries/TemperatureSeriesAnalysis.java
+++ src/main/java/ua/edu/ucu/tempseries/TemperatureSeriesAnalysis.java
@@ -1,24 +1,47 @@
 package ua.edu.ucu.tempseries;
 
+import java.util.InputMismatchException;
+import java.lang.Math;
+
+
 public class TemperatureSeriesAnalysis {
+        private double[] temps;
+        private int size;
+        private int capacity;
+        final double MIN_TEMP = -273.;
 
     public TemperatureSeriesAnalysis() {
-
+        this.size = 0;
+        this.capacity = 0;
     }
 
     public TemperatureSeriesAnalysis(double[] temperatureSeries) {
-
+        this.temps = new double[0];
+        this.size = 0;
+        this.capacity = 0;
+        this.addTemps(temperatureSeries);
     }
 
     public double average() {
-        return -1;
+        this.checkEmpty();
+        double sum = 0;
+        for (double temp : this.temps) {
+            sum+=temp;
+        }
+        return (sum/this.size);
     }
 
     public double deviation() {
-        return 0;
+        double av = this.average();
+        double sum = 0;
+        for (double temp : this.temps) {
+            sum+=(av-temp)*(av-temp);
+        }
+        return Math.sqrt(sum/this.size);
     }
 
     public double min() {
+        
         return 0;
     }
 
@@ -47,6 +70,42 @@ public class TemperatureSeriesAnalysis {
     }
 
     public int addTemps(double... temps) {
-        return 0;
+        this.checkTemps(temps);
+        for (double temp : temps) {
+            this.resizeFill(this.size+1, temp);
+        }
+        return this.size;
+    }
+
+    private void resizeFill(int new_size, double fillWith){
+        if(new_size>this.capacity){
+            int new_capacity = this.capacity;
+            while(new_size>new_capacity){
+                new_capacity = new_capacity*2+1;
+            }
+            double[] new_temps = new double[new_capacity];
+            System.arraycopy(this.temps, 0, new_temps, 0, this.size);
+            this.temps = new_temps;
+        }
+
+        for (int i = this.size; i < new_size; i++) {
+            this.temps[i] = fillWith;
+        }
+        this.size = new_size;
+
+    }
+
+    private void checkTemps(double[] temps){
+        for (double temp : temps) {
+            if(temp<MIN_TEMP){
+                throw new InputMismatchException();
+            }
+        }
+    }
+
+    private void checkEmpty(){
+        if(this.size == 0){
+            throw new IllegalArgumentException();
+        }
     }
 }
--- src/test/java/ua/edu/ucu/tempseries/TemperatureSeriesAnalysisTest.java
+++ src/test/java/ua/edu/ucu/tempseries/TemperatureSeriesAnalysisTest.java
@@ -1,46 +1,77 @@
 package ua.edu.ucu.tempseries;
 
 import static org.junit.Assert.*;
+
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.Ignore;
 
 public class TemperatureSeriesAnalysisTest {
+    TemperatureSeriesAnalysis oneElemAnalysis;
+    TemperatureSeriesAnalysis emptyAnalysis;
+    TemperatureSeriesAnalysis analysis;
+
+    @Before
+    public void setup(){
+        double[] oneElemSeries = {-1.0};
+        oneElemAnalysis = new TemperatureSeriesAnalysis(oneElemSeries);
+        double[] EmptySeries = {};
+        emptyAnalysis = new TemperatureSeriesAnalysis(EmptySeries);
+        double[] temperatureSeries = {3.0, -5.0, 1.0, 5.0};
+        analysis = new TemperatureSeriesAnalysis(temperatureSeries);
+    }
 
     @Test
     public void testAverageWithOneElementArray() {
         // setup input data and expected result
-        double[] temperatureSeries = {-1.0};
-        TemperatureSeriesAnalysis seriesAnalysis = new TemperatureSeriesAnalysis(temperatureSeries);
         double expResult = -1.0;
 
         // call tested method
-        double actualResult = seriesAnalysis.average();
+        double actualResult = analysis.average();
 
         // compare expected result with actual result
         assertEquals(expResult, actualResult, 0.00001);
     }
 
-    @Ignore
     @Test(expected = IllegalArgumentException.class)
     public void testAverageWithEmptyArray() {
-        double[] temperatureSeries = {};
-        TemperatureSeriesAnalysis seriesAnalysis = new TemperatureSeriesAnalysis(temperatureSeries);
-
         // expect exception here
-        seriesAnalysis.average();
+        emptyAnalysis.average();
     }
 
-    @Ignore
+
     @Test
     public void testAverage() {
-        double[] temperatureSeries = {3.0, -5.0, 1.0, 5.0};
-        TemperatureSeriesAnalysis seriesAnalysis = new TemperatureSeriesAnalysis(temperatureSeries);
+
         double expResult = 1.0;
 
-        double actualResult = seriesAnalysis.average();
+        double actualResult = analysis.average();
         
         assertEquals(expResult, actualResult, 0.00001);        
     }
-    
+
+    @Test
+    public void testDeviationWithOneElementArray() {
+        double expResult = -0.0;
+        double actualResult = oneElemAnalysis.deviation();
+        assertEquals(expResult, actualResult, 0.00001);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testDeviationWithEmptyArray() {
+        emptyAnalysis.deviation();
+    }
+
+    @Test
+    public void testDeviation() {
+
+        double expResult = 3.84057287;
+
+        double actualResult = analysis.deviation();
+
+        assertEquals(expResult, actualResult, 0.00001);
+    }
+
+
 
 }
