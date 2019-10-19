2c8dfc22ef12980e98b746efeaa14e5764e67210
ostapdyg <dyhdalovych@ucu.edu.ua>
2019-10-19 09:32:39 +0300

    .

--- src/main/java/ua/edu/ucu/tempseries/TemperatureSeriesAnalysis.java
+++ src/main/java/ua/edu/ucu/tempseries/TemperatureSeriesAnalysis.java
@@ -90,9 +90,9 @@ public class TemperatureSeriesAnalysis {
                 i += 1;
             }
         }
-        double[] new_res = new double[i];
-        System.arraycopy(res, 0, new_res, 0, i);
-        return new_res;
+        double[] newRes = new double[i];
+        System.arraycopy(res, 0, newRes, 0, i);
+        return newRes;
     }
 
     public double[] findTempsGreaterThen(double tempValue) {
@@ -105,9 +105,9 @@ public class TemperatureSeriesAnalysis {
                 i += 1;
             }
         }
-        double[] new_res = new double[i];
-        System.arraycopy(res, 0, new_res, 0, i);
-        return new_res;
+        double[] newRes = new double[i];
+        System.arraycopy(res, 0, newRes, 0, i);
+        return newRes;
     }
 
     public TempSummaryStatistics summaryStatistics() {
@@ -123,21 +123,21 @@ public class TemperatureSeriesAnalysis {
         return this.size;
     }
 
-    private void resizeFill(int new_size, double fillWith) {
-        if (new_size > this.capacity) {
-            int new_capacity = this.capacity;
-            while (new_size > new_capacity) {
-                new_capacity = new_capacity * 2 + 1;
+    private void resizeFill(int newSize, double fillWith) {
+        if (newSize > this.capacity) {
+            int newCapacity = this.capacity;
+            while (newSize > newCapacity) {
+                newCapacity = newCapacity * 2 + 1;
             }
-            double[] new_temps = new double[new_capacity];
-            System.arraycopy(this.temperatures, 0, new_temps, 0, this.size);
-            this.temperatures = new_temps;
+            double[] newTemps = new double[newCapacity];
+            System.arraycopy(this.temperatures, 0, newTemps, 0, this.size);
+            this.temperatures = newTemps;
         }
 
-        for (int i = this.size; i < new_size; i++) {
+        for (int i = this.size; i < newSize; i++) {
             this.temperatures[i] = fillWith;
         }
-        this.size = new_size;
+        this.size = newSize;
 
     }
 
